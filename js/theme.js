0b6418e765205008b704f5b8ccc2b48cb8fa1b06
phelyppe <phelyppesilva@gmail.com>
2021-06-21 22:51:36 -0300

    correção

--- js/theme.js
+++ js/theme.js
@@ -1,18 +1,17 @@
 const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
-prefersColorScheme.addEventListener(mudarTema(prefersColorScheme));
-mudarTema(prefersColorScheme);
-
 function mudarTema(event){
     if (event.matches) {
-        document.getElementById('navbar').style.backgroundColor = 'black';
-        document.getElementById('lbl').style.backgroundColor = 'white';
         alert('pq saporra n tá indo');
+        document.getElementById('navbar').style.backgroundColor = 'rgb(0,0,0)';
+        document.getElementById('lbl').style.backgroundColor = '#FFFFFF';
     }
     else {
-        document.getElementById('navbar').style.backgroundColor = 'white';
-        document.getElementById('lbl').style.backgroundColor = 'black';
         alert('pq saporra n tá indo');
+        document.getElementById('navbar').style.backgroundColor = '#FFFFFF';
+        document.getElementById('lbl').style.backgroundColor = 'rgb(0,0,0)';
     }
 }
 
+prefersColorScheme.addEventListener(mudarTema);
+mudarTema(prefersColorScheme);
