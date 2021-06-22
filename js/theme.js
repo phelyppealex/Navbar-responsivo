4010f7317923854f4c3a0047fc5e9709ff2846ac
phelyppe <phelyppesilva@gmail.com>
2021-06-21 22:42:22 -0300

    belo

--- js/theme.js
+++ js/theme.js
@@ -1,17 +1,18 @@
 const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
-prefersColorScheme.addEventListener(mudarTema);
+prefersColorScheme.addEventListener(mudarTema(prefersColorScheme));
 mudarTema(prefersColorScheme);
 
 function mudarTema(event){
     if (event.matches) {
         document.getElementById('navbar').style.backgroundColor = 'black';
         document.getElementById('lbl').style.backgroundColor = 'white';
+        alert('pq saporra n tá indo');
     }
     else {
         document.getElementById('navbar').style.backgroundColor = 'white';
         document.getElementById('lbl').style.backgroundColor = 'black';
-        alert('pq saporra n tá indo?');
+        alert('pq saporra n tá indo');
     }
 }
 
