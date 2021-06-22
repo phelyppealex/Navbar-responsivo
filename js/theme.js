84c0b249dca8fc05dfc72d1a44e466a2ae7ba602
phelyppe <phelyppesilva@gmail.com>
2021-06-21 22:58:08 -0300

    dark

--- js/theme.js
+++ js/theme.js
@@ -2,12 +2,12 @@ const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
 function mudarTema(event){
     if (event.matches) {
-        alert('pq saporra n tá indo');
+        alert('dark');
         document.getElementById('navbar').style.backgroundColor = 'rgb(0,0,0)';
         document.getElementById('lbl').style.backgroundColor = '#FFFFFF';
     }
     else {
-        alert('pq saporra n tá indo');
+        alert('white');
         document.getElementById('navbar').style.backgroundColor = '#FFFFFF';
         document.getElementById('lbl').style.backgroundColor = 'rgb(0,0,0)';
     }
