eeb10af212782607f54d84353c397655f848e258
phelyppe <phelyppesilva@gmail.com>
2021-06-23 00:27:02 -0300

    formatando código

--- js/theme.js
+++ js/theme.js
@@ -4,12 +4,10 @@ var qualquer = setInterval(mudarTema(prefersColorScheme), 1000);
 
 function mudarTema(event){
     if (event.matches) {
-        alert('dark');
         document.getElementById('navbar').style.backgroundColor = 'rgb(0,0,0)';
         document.getElementById('lbl').style.color = '#FFFFFF';
     }
     else {
-        alert('white');
         document.getElementById('navbar').style.backgroundColor = '#FFFFFF';
         document.getElementById('lbl').style.color = 'rgb(0,0,0)';
     }
