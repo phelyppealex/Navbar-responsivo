74b7261bac3f2b4745a07d713fe3bb5b97aa6a4e
phelyppe <phelyppesilva@gmail.com>
2021-06-26 22:43:49 -0300

    changer theme

--- js/theme.js
+++ js/theme.js
@@ -1,18 +1,41 @@
+const preto = 'rgb(0,0,0)';
+const branco = '#FFFF';
+const cinza = 'rgb(216, 216, 216)';
+const listaDark = 'rgb(22, 22, 22)';
+const listaLight = 'gray';
+
 var dark = true;
 
+
 function mudarTema(){
-    var preto = 'rgb(0,0,0)';
-    var branco = 'rgb(216, 216, 216)';
-    
     if (dark) {
         dark = false;
-        document.querySelector('#navbar').style.backgroundColor = branco;
+        document.querySelector('#navbar').style.backgroundColor = cinza;
         document.querySelector('#lbl').style.color = preto;
         document.querySelector('.btn').style.color = preto;
+        document.querySelector('.lista').style.backgroundColor = listaLight;
     }else{
         dark = true;
         document.querySelector('#navbar').style.backgroundColor = preto;
         document.querySelector('#lbl').style.color = branco;
         document.querySelector('.btn').style.color = branco;
+        document.querySelector('.lista').style.backgroundColor = listaDark;
+    }
+
+    resize();
+}
+
+function resize(){
+    var listaDark = 'rgb(22, 22, 22)';
+    var listaLight = 'gray';
+    
+    if(window.visualViewport.width > 820){
+        document.querySelector('.lista').style.backgroundColor = 'rgb(0,0,0,0)';
+    }else{
+        if (dark) {
+            document.querySelector('.lista').style.backgroundColor = listaDark;
+        }else{
+            document.querySelector('.lista').style.backgroundColor = listaLight;
+        }
     }
 }
